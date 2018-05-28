5f0817761cf7c5d27135e7156544fd77540acdd6
naruteride <tadanide@gmail.com>
2018-05-28 23:12:33 +0900

    많은것을 수정함

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,20 +4,24 @@ onload = function () {
     var navCover = document.querySelector('#nav-cover');
     var buttonCover = document.querySelector('#button-cover');
     var title = document.querySelector('#title');
+    var nav = document.querySelector('#nav');
 
 
     bagRamen.addEventListener("click", function() {
         bagRamen.classList.add('button-decreasing');
         navCover.classList.add('growing');
         buttonCover.classList.add('button-cover-growing');
-        title.classList.add("title-growing");
+        title.classList.add('title-diminish');
+        cupRamen.classList.add('fade');
+        nav.classList.add('nav-diminish');
     })
 
     cupRamen.addEventListener("click", function () {
         cupRamen.classList.add('button-decreasing');
         navCover.classList.add('growing');
         buttonCover.classList.add('button-cover-growing');
-        title.classList.add("title-growing");
+        title.classList.add('title-diminish');
+        bagRamen.classList.add('fade');
+        nav.classList.add('nav-diminish');
     })
-
 }
